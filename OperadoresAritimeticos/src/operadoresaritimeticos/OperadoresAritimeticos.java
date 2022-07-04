a27ab3ac3cb578b8843d066db73735bb2011294d
Mateus Ferraz <mateus.p.ferraz@gmail.com>
2022-07-04 18:49:40 -0300

    Operadores Aritiméticos

---
+++ OperadoresAritimeticos/src/operadoresaritimeticos/OperadoresAritimeticos.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package operadoresaritimeticos;
+
+/**
+ *
+ * @author Mateus Ferraz
+ */
+public class OperadoresAritimeticos {
+
+  
+    public static void main(String[] args) {
+        /* TODO code application logic here
+       int n1 = 3;
+       int n2 = 5;
+       float m = (n1+n2)/2;
+        System.out.println("A média é igual a "+ m);
+        */
+/* int numero = 5; 
+ int valor = 5 + ++numero;
+        System.out.println( valor);
+      
+      
+int x = 4; 
+x+=2; // x= x *2
+System.out.println(x);
+  
+float v = 8.5f ;
+   int ar = (int) Math.round(v);
+        System.out.println(ar);
+*/
+double ale = Math.random();
+int n = (int) (50 + ale* (100-50) );
+        System.out.println(n);
+    }
+    
+}
